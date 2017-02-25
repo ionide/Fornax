1b35ca75d2a3c81f6121e45eb58b0af7f623985c
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2017-02-25 23:28:45 +0100

    Fix sample

--- samples/generator.fsx
+++ samples/generator.fsx
@@ -1,4 +1,4 @@
 #load "../paket-files/include-scripts/net46/include.main.group.fsx"
-#r "../build/Fornax.dll"
+#r "../build/Fornax.Core.dll"
 let path = System.IO.Path.GetFullPath "samples"
-Fornax.generateFolder path
+Generator.generateFolder path
