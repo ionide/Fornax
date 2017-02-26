31598a72dc2c8c778b501295200cd287b293bb81
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2017-02-26 12:14:35 +0100

    Update build process

--- build.fsx
+++ build.fsx
@@ -2,21 +2,55 @@
 #r "./packages/FAKE/tools/FakeLib.dll"
 
 open Fake
+open Fake.AssemblyInfoFile
+open Fake.ReleaseNotesHelper
 
-// Directories
+let project = "Fornax"
+let summary = "Fornax is a static site generator using type safe F# DSL to define page templates"
+let release = LoadReleaseNotes "CHANGELOG.md"
 let buildDir  = "./build/"
-let deployDir = "./deploy/"
-
-
-// Filesets
-let appReferences  =
-    !!  "/**/*.fsproj"
+let appReferences = !!  "src/**/*.fsproj"
 
 
 // Targets
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; deployDir]
+    CleanDirs [buildDir;]
 )
+
+let (|Fsproj|Csproj|Vbproj|) (projFileName:string) =
+    match projFileName with
+    | f when f.EndsWith("fsproj") -> Fsproj
+    | f when f.EndsWith("csproj") -> Csproj
+    | f when f.EndsWith("vbproj") -> Vbproj
+    | _                           -> failwith (sprintf "Project file %s not supported. Unknown project type." projFileName)
+
+// Generate assembly info files with the right version & up-to-date information
+Target "AssemblyInfo" (fun _ ->
+    let getAssemblyInfoAttributes projectName =
+        [ Attribute.Title (projectName)
+          Attribute.Product project
+          Attribute.Description summary
+          Attribute.Version release.AssemblyVersion
+          Attribute.FileVersion release.AssemblyVersion ]
+
+    let getProjectDetails projectPath =
+        let projectName = System.IO.Path.GetFileNameWithoutExtension(projectPath)
+        ( projectPath,
+          projectName,
+          System.IO.Path.GetDirectoryName(projectPath),
+          (getAssemblyInfoAttributes projectName)
+        )
+
+    appReferences
+    |> Seq.map getProjectDetails
+    |> Seq.iter (fun (projFileName, projectName, folderName, attributes) ->
+        match projFileName with
+        | Fsproj -> CreateFSharpAssemblyInfo (folderName @@ "AssemblyInfo.fs") attributes
+        | Csproj -> CreateCSharpAssemblyInfo ((folderName @@ "Properties") @@ "AssemblyInfo.cs") attributes
+        | Vbproj -> CreateVisualBasicAssemblyInfo ((folderName @@ "My Project") @@ "AssemblyInfo.vb") attributes
+        )
+)
+
 
 Target "Build" (fun _ ->
     // compile all projects below src/app/
@@ -24,16 +58,10 @@ Target "Build" (fun _ ->
     |> Log "AppBuild-Output: "
 )
 
-Target "Deploy" (fun _ ->
-    !! (buildDir + "/**/*.*")
-    -- "*.zip"
-    |> Zip buildDir (deployDir + "ApplicationName.zip")
-)
-
 // Build order
 "Clean"
+  ==> "AssemblyInfo"
   ==> "Build"
-  ==> "Deploy"
 
 // start build
 RunTargetOrDefault "Build"
