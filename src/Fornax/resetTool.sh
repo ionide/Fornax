60403a8ec3ecfff15bebc149dfd3f78dd95cef12
Nat Elkins <nathaniel.elkins@gmail.com>
2019-07-04 14:02:00 -0400

    Fixing template.

--- src/Fornax/resetTool.sh
+++ src/Fornax/resetTool.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 dotnet tool uninstall -g Fornax
-dotnet pack -c release -o nupkg
+dotnet pack -c Release -o nupkg
 dotnet tool install --add-source ./nupkg -g fornax
 echo "Finished fornax reset"
