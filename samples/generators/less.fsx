c9de28e19ec105599dfe1bf376deaa451653513e
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2020-02-18 23:48:16 +0100

    Update FCS to latest

--- samples/generators/less.fsx
+++ samples/generators/less.fsx
@@ -1,4 +1,6 @@
 #r "../../src/Fornax.Core/bin/Release/netstandard2.0/Fornax.Core.dll"
+#r "../_lib/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+#r "../_lib/Microsoft.Extensions.DependencyInjection.dll"
 #r "../_lib/dotless.Core.dll"
 
 open System.IO
@@ -7,5 +9,10 @@ let parseLess fileContent =
     dotless.Core.Less.Parse fileContent
 
 let generate (ctx : SiteContents) (projectRoot: string) (page: string) =
-    let inputPath = Path.Combine(projectRoot, page)
-    File.ReadAllText inputPath |> parseLess
+    try
+        let inputPath = Path.Combine(projectRoot, page)
+        File.ReadAllText inputPath |> parseLess
+    with
+    | ex ->
+        printfn "ERR: %s" ex.Message
+        ""
