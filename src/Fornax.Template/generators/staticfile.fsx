eadb79ecbee2a4cd3b9d59b9e8b77889558c744d
Robert Pickering <robertfpickering@fastmail.com>
2020-04-08 06:52:17 +0200

    use new functionality in the template

--- src/Fornax.Template/generators/staticfile.fsx
+++ src/Fornax.Template/generators/staticfile.fsx
@@ -4,4 +4,4 @@ open System.IO
 
 let generate (ctx : SiteContents) (projectRoot: string) (page: string) =
     let inputPath = Path.Combine(projectRoot, page)
-    File.ReadAllText inputPath
+    File.ReadAllBytes inputPath  
