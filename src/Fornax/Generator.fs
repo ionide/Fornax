ionide/Fornax

commit 0a7c6699f6f96b6813de53b6a6c80f5c6f0c200d

Improve perf by caching generators (#65)
Small change, order of magintude improvement. Tested on 185 page blog:

Pre-change - Generation time: 00:04:59.3250690
Post-change - Generation time: 00:00:19.2490206

Co-authored-by: Robert Pickering <robertfpickering@fastmail.com>

src/Fornax/Generator.fs
@@ -2,6 +2,8 @@
 module Generator
 
 open System
+open System.Collections.Concurrent
+open System.Diagnostics
 open System.IO
 open System.Text
 open Config
@@ -166,15 +168,30 @@ module GeneratorEvaluator =
                 |> fun s -> s.Trim(Environment.NewLine.ToCharArray())
             Error completeErrorReport
 
+    let private generatorCache = new ConcurrentDictionary<_,_>()
+
+    /// allows the cache to be cleared when running in watch mode and a change is detected
+    let removeItemFromGeneratorCache() =
+        generatorCache.Clear()
 
     ///`generatorPath` - absolute path to `.fsx` file containing the generator
     ///`projectRoot` - path to root of the site project
     ///`page` - path to the file that should be transformed
     let evaluate (fsi : FsiEvaluationSession) (siteContent : SiteContents) (generatorPath : string) (projectRoot: string) (page: string)  =
-        getGeneratorContent fsi generatorPath
-        |> Result.bind (fun ft ->
-            let generator = compileExpression ft
 
+        let ok, generator = generatorCache.TryGetValue(generatorPath)
+        let generator =
+            if ok then
+                generator
+            else
+                let generator =
+                    getGeneratorContent fsi generatorPath
+                    |> Result.bind (compileExpression >> Ok)
+                generatorCache.AddOrUpdate(generatorPath, generator, fun key value -> value) |> ignore
+                generator
+
+        generator
+        |> Result.bind (fun generator ->
             let result = invokeFunction generator [box siteContent; box projectRoot; box page ]
 
             result
@@ -423,6 +440,7 @@ module Logger =
 
 ///`projectRoot` - path to the root of website
 let generateFolder (projectRoot : string) (isWatch: bool) =
+    let sw = Stopwatch.StartNew()
 
     let relative toPath fromPath =
         let toUri = Uri(toPath)
@@ -480,3 +498,5 @@ let generateFolder (projectRoot : string) (isWatch: bool) =
             |> relative projectRoot
             |> generate fsi config sc projectRoot
             |> logResult)
+
+        Logger.informationfn "Generation time: %A" sw.Elapsed

src/Fornax/Fornax.fs
@@ -67,6 +67,8 @@ let createFileWatcher dir handler =
     /// Adding handler to trigger websocket/live refresh
     let contentChangedHandler _ =
         signalContentChanged.Trigger(Choice<unit,Error>.Choice1Of2 ())
+        GeneratorEvaluator.removeItemFromGeneratorCache()
+
     signalContentChanged.Trigger(Choice<unit,Error>.Choice1Of2 ())
     fileSystemWatcher.Created.Add contentChangedHandler
     fileSystemWatcher.Changed.Add contentChangedHandler

commit bd61de1beb31feba8059075230333f850a3bef3a

Always call 'getGeneratorContent' for posts in watch mode
Fixes a regression introduced in 0a7c6699

src/Fornax/Generator.fs
@@ -177,11 +177,15 @@ module GeneratorEvaluator =
     ///`generatorPath` - absolute path to `.fsx` file containing the generator
     ///`projectRoot` - path to root of the site project
     ///`page` - path to the file that should be transformed
-    let evaluate (fsi : FsiEvaluationSession) (siteContent : SiteContents) (generatorPath : string) (projectRoot: string) (page: string)  =
+    ///`isWatch` - status of watch mode
+    let evaluate (fsi : FsiEvaluationSession) (siteContent : SiteContents) (generatorPath : string) (projectRoot: string) (page: string) (isWatch: bool) =
 
         let ok, generator = generatorCache.TryGetValue(generatorPath)
+        let pageExt = (Path.GetExtension page).ToLower()
+        let skipCache = isWatch && List.contains pageExt [".md"; ".mkd"; ".markdown"]
+
         let generator =
-            if ok then
+            if ok && not skipCache then
                 generator
             else
                 let generator =
@@ -328,14 +332,14 @@ let pickGenerator (cfg: Config.Config)  (siteContent : SiteContents) (projectRoo
 
 ///`projectRoot` - path to the root of website
 ///`page` - path to page that should be generated
-let generate fsi (cfg: Config.Config) (siteContent : SiteContents) (projectRoot : string) (page : string) =
+let generate fsi (cfg: Config.Config) (siteContent : SiteContents) (projectRoot : string) (page : string) (isWatch: bool) =
     let startTime = DateTime.Now
     match pickGenerator cfg siteContent projectRoot page with
     | None ->
         GeneratorIgnored
     | Some (Simple(layoutPath, outputPath)) ->
 
-        let result = GeneratorEvaluator.evaluate fsi siteContent layoutPath projectRoot page
+        let result = GeneratorEvaluator.evaluate fsi siteContent layoutPath projectRoot page isWatch
         match result with
         | Ok r ->
             let dir = Path.GetDirectoryName outputPath
@@ -385,7 +389,7 @@ let runOnceGenerators fsi (cfg: Config.Config) (siteContent : SiteContents) (pro
         | NewFileName newFileName ->
 
             let outputPath = Path.Combine(projectRoot, "_public", newFileName)
-            let result = GeneratorEvaluator.evaluate fsi siteContent generatorPath projectRoot ""
+            let result = GeneratorEvaluator.evaluate fsi siteContent generatorPath projectRoot "" false
             match result with
             | Ok r ->
                 let dir = Path.GetDirectoryName outputPath
@@ -493,9 +497,8 @@ let generateFolder (projectRoot : string) (isWatch: bool) =
 
         Directory.GetFiles(projectRoot, "*", SearchOption.AllDirectories)
         |> Array.iter (fun filePath ->
-            filePath
-            |> relative projectRoot
-            |> generate fsi config sc projectRoot
+            (relative projectRoot filePath, isWatch)
+            ||> generate fsi config sc projectRoot
             |> logResult)
 
         Logger.informationfn "Generation time: %A" sw.Elapsed