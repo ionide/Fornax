21f65d7045099d4dd1e760456c686376535292d7
Chet Husk <chusk3@gmail.com>
2022-01-15 13:14:18 -0600

    pack with sourcelink and changelogs

--- build.fsx
+++ build.fsx
@@ -34,7 +34,6 @@ let gitRaw = Environment.environVarOrDefault "gitRaw" ("https://raw.github.com/"
 // --------------------------------------------------------------------------------------
 
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-let release = ReleaseNotes.parse (System.IO.File.ReadAllLines "CHANGELOG.md")
 
 let packageDir = __SOURCE_DIRECTORY__ </> "out"
 let buildDir = __SOURCE_DIRECTORY__ </> "temp"
@@ -112,45 +111,6 @@ Target.create "Pack" (fun _ ->
         }) "src/Fornax"
 )
 
-Target.create "ReleaseGitHub" (fun _ ->
-    let remote =
-        Git.CommandHelper.getGitResult "" "remote -v"
-        |> Seq.filter (fun (s: string) -> s.EndsWith("(push)"))
-        |> Seq.tryFind (fun (s: string) -> s.Contains(gitOwner + "/" + gitName))
-        |> function None -> gitHome + "/" + gitName | Some (s: string) -> s.Split().[0]
-
-    Git.Staging.stageAll ""
-    Git.Commit.exec "" (sprintf "Bump version to %s" release.NugetVersion)
-    Git.Branches.pushBranch "" remote (Git.Information.getBranchName "")
-
-
-    Git.Branches.tag "" release.NugetVersion
-    Git.Branches.pushTag "" remote release.NugetVersion
-
-    let client =
-        let user =
-            match getBuildParam "github-user" with
-            | s when not (isNullOrWhiteSpace s) -> s
-            | _ -> UserInput.getUserInput "Username: "
-        let pw =
-            match getBuildParam "github-pw" with
-            | s when not (isNullOrWhiteSpace s) -> s
-            | _ -> UserInput.getUserPassword "Password: "
-
-        // Git.createClient user pw
-        GitHub.createClient user pw
-    let files = !! (packageDir </> "*.nupkg")
-
-    // release on github
-    let cl =
-        client
-        |> GitHub.draftNewRelease gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes
-    (cl,files)
-    ||> Seq.fold (fun acc e -> acc |> GitHub.uploadFile e)
-    |> GitHub.publishDraft//releaseDraft
-    |> Async.RunSynchronously
-)
-
 Target.create "Push" (fun _ ->
     let key =
         match getBuildParam "nuget-key" with
@@ -178,7 +138,6 @@ Target.create "Release" DoNothing
 
 "Default"
   ==> "Pack"
-  ==> "ReleaseGitHub"
   ==> "Push"
   ==> "Release"
 
