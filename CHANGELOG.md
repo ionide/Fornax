9102720fd95f5b3871733174453aa1c66367725a
Chet Husk <baronfel@users.noreply.github.com>
2022-01-15 17:19:02 -0600

    better release script (and overall updates) (#102)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,37 +1,73 @@
-### 0.13.1 - 24.04.2020
+# Changelog
+
+## 0.14.0 - 15.01.2022
+
+### Added
+
+* Update to FCS 41
+* Add Sourcelink to the core library
+* Support .NET 5 and .NET 6
+* typo fix for the About page
+* Trim file paths with a platform-agnostic path separator (#91)
+* Add some Generator documentation.
+* Minor readme grammatical fixes (#83)
+* Misc fixes (#80)
+* Watch mode enhancement (#79)
+* Update to .NET 5 (#78)
+* Change after testing on Windows Directory.Delete throws exception on files with read-only attribute.
+* Add LibGit2Sharp, add sub commands to fornax new
+* Update FCS, fake-cli and paket. (#73)
+* Add GitHub Actions build (#74)
+* Improve mobile layout
+* allow the user to configure pages size in the global loader
+* Add support for paging
+* Improve perf by caching generators (#65)
+* Default template does not support on sub-dirs (#64)
+* fix typo (#62)
+
+## 0.13.1 - 24.04.2020
+
+### Added
 * Update to FCS 35.0 (by [@Krzysztof-Cieslak](https://github.com/Krzysztof-Cieslak))
 * Pass `WATCH` define in case of watch mode (by [@Krzysztof-Cieslak](https://github.com/Krzysztof-Cieslak))
 
-### 0.13.0 - 20.04.2020
+## 0.13.0 - 20.04.2020
+### Added
 * Add way to check developments to the template (by [@robertpi](https://github.com/robertpi))
 * Summarize posts using more marker (by [@robertpi](https://github.com/robertpi))
 * Update to FCS 34.1 (by [@Krzysztof-Cieslak](https://github.com/Krzysztof-Cieslak))
 
-### 0.12.0 - 14.04.2020
+## 0.12.0 - 14.04.2020
+### Added
 * WebSocket refresh uses excesive CPU (by [@robertpi](https://github.com/robertpi))
 * Allow generate to return a byte array (by [@robertpi](https://github.com/robertpi))
 * Refactor template (by [@robertpi](https://github.com/robertpi))
 
-### 0.11.1 - 07.04.2020
+## 0.11.1 - 07.04.2020
+### Added
 * Fix for once generator running even if not found (by [@sasmithjr](https://github.com/sasmithjr))
 * Use exceptions .ToString() when printing error (by [@robertpi](https://github.com/robertpi))
 
-### 0.11.0 - 02.03.2020
+## 0.11.0 - 02.03.2020
+### Added
 * Add a port argument to the watch command  (by [@toburger](https://github.com/toburger))
 * Collect and propogate loader errors (by [@jbeeko](https://github.com/jbeeko))
 * Add MultipleFiles as possible generator output (by [@Krzysztof-Cieslak](https://github.com/Krzysztof-Cieslak))
 
-### 0.10.0 - 20.02.2020
+## 0.10.0 - 20.02.2020
+### Added
 * Update to .NET Core 3.0
 * Adds tools manifest .config/dotnet-tools.json
 * Redesign Fornax around idea of `loaders` and `generators`
 * Create new `fornax new` template
 
-### 0.2.0 - 05.08.2019
+## 0.2.0 - 05.08.2019
+### Added
 * Update to .Net Core
 * Distribute as .Net Core global tool
 
-### 0.1.0 - 04.06.2017
+## 0.1.0 - 04.06.2017
+### Added
 * Defining templates in F# DSL
 * Creating pages using templates from `.md` files with `layout` entry
 * Creating plain pages without templates from `md` files without `layout` entry
--- build.fsx
+++ build.fsx
@@ -34,7 +34,6 @@ let gitRaw = Environment.environVarOrDefault "gitRaw" ("https://raw.github.com/"
 // --------------------------------------------------------------------------------------
 
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-let release = ReleaseNotes.parse (System.IO.File.ReadAllLines "CHANGELOG.md")
 
 let packageDir = __SOURCE_DIRECTORY__ </> "out"
 let buildDir = __SOURCE_DIRECTORY__ </> "temp"
@@ -64,33 +63,6 @@ let DoNothing = ignore
 
 Target.create "Clean" (fun _ ->
     Shell.cleanDirs [buildDir; packageDir]
-)
-
-Target.create "AssemblyInfo" (fun _ ->
-    let getAssemblyInfoAttributes projectName =
-        [ AssemblyInfo.Title projectName
-          AssemblyInfo.Product project
-          AssemblyInfo.Description summary
-          AssemblyInfo.Version release.AssemblyVersion
-          AssemblyInfo.FileVersion release.AssemblyVersion ]
-
-    let getProjectDetails projectPath =
-        let projectName = System.IO.Path.GetFileNameWithoutExtension(projectPath)
-        ( projectPath,
-          projectName,
-          System.IO.Path.GetDirectoryName(projectPath),
-          (getAssemblyInfoAttributes projectName)
-        )
-
-    !! "src/**/*.??proj"
-    |> Seq.map getProjectDetails
-    |> Seq.iter (fun (projFileName, _, folderName, attributes) ->
-        match projFileName with
-        | proj when proj.EndsWith("fsproj") -> AssemblyInfoFile.createFSharp (folderName </> "AssemblyInfo.fs") attributes
-        | proj when proj.EndsWith("csproj") -> AssemblyInfoFile.createCSharp ((folderName </> "Properties") </> "AssemblyInfo.cs") attributes
-        | proj when proj.EndsWith("vbproj") -> AssemblyInfoFile.createVisualBasic ((folderName </> "My Project") </> "AssemblyInfo.vb") attributes
-        | _ -> ()
-        )
 )
 
 Target.create "Restore" (fun _ ->
@@ -132,61 +104,11 @@ Target.create "TestTemplate" (fun _ ->
 // --------------------------------------------------------------------------------------
 
 Target.create "Pack" (fun _ ->
-
-    //Pack Fornax global tool
-    Environment.setEnvironVar "PackageVersion" release.NugetVersion
-    Environment.setEnvironVar "Version" release.NugetVersion
-    Environment.setEnvironVar "Authors" "Krzysztof Cieslak"
-    Environment.setEnvironVar "Description" summary
-    Environment.setEnvironVar "PackageReleaseNotes" (release.Notes |> String.toLines)
-    Environment.setEnvironVar "PackageTags" "f#, site-generator, html"
-    Environment.setEnvironVar "PackageProjectUrl" "https://github.com/Ionide/Fornax"
-    Environment.setEnvironVar "PackageLicenseExpression" "MIT"
-
     DotNet.pack (fun p ->
         { p with
             OutputPath = Some packageDir
             Configuration = DotNet.BuildConfiguration.Release
         }) "src/Fornax"
-)
-
-Target.create "ReleaseGitHub" (fun _ ->
-    let remote =
-        Git.CommandHelper.getGitResult "" "remote -v"
-        |> Seq.filter (fun (s: string) -> s.EndsWith("(push)"))
-        |> Seq.tryFind (fun (s: string) -> s.Contains(gitOwner + "/" + gitName))
-        |> function None -> gitHome + "/" + gitName | Some (s: string) -> s.Split().[0]
-
-    Git.Staging.stageAll ""
-    Git.Commit.exec "" (sprintf "Bump version to %s" release.NugetVersion)
-    Git.Branches.pushBranch "" remote (Git.Information.getBranchName "")
-
-
-    Git.Branches.tag "" release.NugetVersion
-    Git.Branches.pushTag "" remote release.NugetVersion
-
-    let client =
-        let user =
-            match getBuildParam "github-user" with
-            | s when not (isNullOrWhiteSpace s) -> s
-            | _ -> UserInput.getUserInput "Username: "
-        let pw =
-            match getBuildParam "github-pw" with
-            | s when not (isNullOrWhiteSpace s) -> s
-            | _ -> UserInput.getUserPassword "Password: "
-
-        // Git.createClient user pw
-        GitHub.createClient user pw
-    let files = !! (packageDir </> "*.nupkg")
-
-    // release on github
-    let cl =
-        client
-        |> GitHub.draftNewRelease gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes
-    (cl,files)
-    ||> Seq.fold (fun acc e -> acc |> GitHub.uploadFile e)
-    |> GitHub.publishDraft//releaseDraft
-    |> Async.RunSynchronously
 )
 
 Target.create "Push" (fun _ ->
@@ -203,7 +125,6 @@ Target.create "Default" DoNothing
 Target.create "Release" DoNothing
 
 "Clean"
-  ==> "AssemblyInfo"
   ==> "Restore"
   ==> "Build"
   ==> "Publish"
@@ -217,7 +138,6 @@ Target.create "Release" DoNothing
 
 "Default"
   ==> "Pack"
-  ==> "ReleaseGitHub"
   ==> "Push"
   ==> "Release"
 
