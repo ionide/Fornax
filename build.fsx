32edbcdae6f2103d2af8f3d93dca6114785ba0d5
Chet Husk <baronfel@users.noreply.github.com>
2022-05-05 09:04:01 -0500

    Push from packageDir, not buildDir

--- build.fsx
+++ build.fsx
@@ -116,7 +116,7 @@ Target.create "Push" (fun _ ->
         match getBuildParam "nuget-key" with
         | s when not (isNullOrWhiteSpace s) -> s
         | _ -> UserInput.getUserPassword "NuGet Key: "
-    Paket.push (fun p -> { p with WorkingDir = buildDir; ApiKey = key }))
+    Paket.push (fun p -> { p with WorkingDir = packageDir; ApiKey = key }))
 
 // --------------------------------------------------------------------------------------
 // Build order
